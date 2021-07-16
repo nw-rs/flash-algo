5570ed42a4cc923642721ee4ea1647c9ac221fa3
willemml <willem@leit.so>
2021-07-15 23:11:35 -0700

    reimplement qspi code from rustworks using pointers to registers instead of peripherals

--- src/main.rs
+++ src/main.rs
@@ -5,73 +5,240 @@
 mod algo;
 
 use core::mem::MaybeUninit;
-use rustworks::external_flash::ExternalFlash;
-use rustworks::external_flash::Indirect;
-use rustworks::stm32f7xx_hal::delay::Delay;
-use rustworks::stm32f7xx_hal::gpio::GpioExt;
-use rustworks::stm32f7xx_hal::gpio::Speed;
-use rustworks::stm32f7xx_hal::pac::Peripherals;
-use rustworks::stm32f7xx_hal::prelude::*;
-use rustworks::stm32f7xx_hal::rcc::{HSEClock, HSEClockMode};
+
+use cortex_m::asm;
+use stm32f7xx_hal::pac::{QUADSPI, RCC};
 
 use self::algo::*;
 
-struct NumWorksAlgo {
-    external_flash: ExternalFlash<Indirect>,
+const FLASH_ADDRESS_SIZE: u8 = 23;
+
+//#[allow(dead_code)]
+#[repr(u8)]
+enum QspiWidth {
+    Single = 0b01,
+    Quad = 0b11,
 }
+
+/// The different QSPI functional modes.
+#[repr(u8)]
+enum QspiMode {
+    IndirectWrite = 0b00,
+    IndirectRead = 0b01,
+}
+
+/// The number of bytes required to specify addresses on the chip.
+#[repr(u8)]
+enum QspiSize {
+    OneByte = 0b00,
+    ThreeBytes = 0b10,
+}
+
+/// Commands (instructions) that can be sent to the flash chip.
+#[repr(u8)]
+enum Command {
+    ReadStatusRegister1 = 0x05,
+    WriteStatusRegister2 = 0x31,
+    WriteEnable = 0x06,
+    WriteEnableVolatile = 0x50,
+    PageProgram = 0x02,
+    EnableQPI = 0x38,
+    ChipErase = 0xC7,
+    Erase64KbyteBlock = 0xD8,
+    ReleaseDeepPowerDown = 0xAB,
+}
+
+struct NumWorksAlgo;
 
 algo!(NumWorksAlgo);
 
 impl FlashAlgo for NumWorksAlgo {
     fn new(_address: u32, _clock: u32, _function: u32) -> Result<Self, ErrorCode> {
-        let mut peripherals = Peripherals::take().unwrap();
-        let cp = cortex_m::Peripherals::take().unwrap();
+        unsafe {
+            let rcc = &(*RCC::ptr());
+            rcc.ahb3enr.modify(|_, w| w.qspien().set_bit());
 
-        let gpiob = peripherals.GPIOB.split();
-        let gpioc = peripherals.GPIOC.split();
-        let gpiod = peripherals.GPIOD.split();
-        let gpioe = peripherals.GPIOE.split();
+            let qspi = &(*QUADSPI::ptr());
+            // Single flash mode with a QSPI clock prescaler of 2 (216 / 2 = 108 MHz), FIFO
+            // threshold only matters for DMA and is set to 4 to allow word sized DMA requests
 
-        let qspi_pins = (
-            gpiob.pb2.into_alternate_af9().set_speed(Speed::VeryHigh),
-            gpiob.pb6.into_alternate_af10().set_speed(Speed::VeryHigh),
-            gpioc.pc9.into_alternate_af9().set_speed(Speed::VeryHigh),
-            gpiod.pd12.into_alternate_af9().set_speed(Speed::VeryHigh),
-            gpiod.pd13.into_alternate_af9().set_speed(Speed::VeryHigh),
-            gpioe.pe2.into_alternate_af9().set_speed(Speed::VeryHigh),
-        );
+            // Configure controller for flash chip.
+            qspi.dcr.write_with_zero(|w| {
+                w.fsize()
+                    .bits(FLASH_ADDRESS_SIZE - 1)
+                    .csht()
+                    .bits(2)
+                    .ckmode()
+                    .set_bit()
+            });
 
-        let external_flash =
-            ExternalFlash::new(&mut peripherals.RCC, peripherals.QUADSPI, qspi_pins);
+            qspi.cr
+                .write_with_zero(|w| w.prescaler().bits(3).en().set_bit());
+        }
+        // Turn on the chip.
+        send_spi_command(Command::ReleaseDeepPowerDown, None);
 
-        let rcc = peripherals.RCC.constrain();
-        let clocks = rcc
-            .cfgr
-            .hse(HSEClock::new(8.mhz(), HSEClockMode::Oscillator))
-            .use_pll()
-            .sysclk(rustworks::HCLK.hz())
-            .freeze();
-        let mut delay = Delay::new(cp.SYST, clocks);
+        for _ in 0..100000 {
+            asm::nop();
+        }
 
-        Ok(Self {
-            external_flash: external_flash.init(&mut delay),
-        })
+        // Enable writing to the chip so that the status register can be changed.
+        send_spi_command(Command::WriteEnableVolatile, None);
+
+        // Set QPI to enabled in the chip's status register.
+        send_spi_command(Command::WriteStatusRegister2, Some(0x02));
+
+        // Enable QPI on the chip.
+        send_spi_command(Command::EnableQPI, None);
+
+        Ok(Self)
     }
 
     fn erase_all(&mut self) -> Result<(), ErrorCode> {
-        self.external_flash.chip_erase();
+        qpi_command(Command::WriteEnable);
+        qpi_command(Command::ChipErase);
         Ok(())
     }
 
     fn erase_sector(&mut self, addr: u32) -> Result<(), ErrorCode> {
-        self.external_flash.block_erase_4k(addr);
+        let qspi = unsafe { &(*QUADSPI::ptr()) };
+        qspi.ccr.write(|w| unsafe {
+            w.fmode()
+                .bits(QspiMode::IndirectWrite as u8)
+                .imode()
+                .bits(QspiWidth::Quad as u8)
+                .admode()
+                .bits(QspiWidth::Quad as u8)
+                .adsize()
+                .bits(QspiSize::ThreeBytes as u8)
+                .instruction()
+                .bits(Command::Erase64KbyteBlock as u8)
+        });
+
+        qspi.ar.write(|w| unsafe { w.bits(addr) });
+
+        while qspi.sr.read().busy().bit_is_set() {
+            asm::nop();
+        }
+
+        wait_busy();
         Ok(())
     }
 
     fn program_page(&mut self, addr: u32, size: u32, data: *const u8) -> Result<(), ErrorCode> {
-        self.external_flash.program_page(addr, unsafe {
-            core::slice::from_raw_parts(data, size as usize)
+        let qspi = unsafe { &(*QUADSPI::ptr()) };
+        let data = unsafe { core::slice::from_raw_parts(data, size as usize) };
+        assert!(!data.is_empty());
+
+        qspi.dlr
+            .write(|w| unsafe { w.dl().bits(data.len() as u32 - 1) });
+
+        qspi.ccr.write(|w| unsafe {
+            w.fmode()
+                .bits(QspiMode::IndirectWrite as u8)
+                .imode()
+                .bits(QspiWidth::Quad as u8)
+                .dmode()
+                .bits(QspiWidth::Quad as u8)
+                .admode()
+                .bits(QspiWidth::Quad as u8)
+                .adsize()
+                .bits(QspiSize::ThreeBytes as u8)
+                .instruction()
+                .bits(Command::PageProgram as u8)
         });
+
+        qspi.ar.write(|w| unsafe { w.bits(addr) });
+
+        for byte in data {
+            // while self.qspi.sr.read().ftf().bit_is_clear() {
+            //     asm::nop();
+            // }
+            unsafe {
+                core::ptr::write_volatile(&qspi.dr as *const _ as *mut u8, *byte);
+            }
+        }
+
+        while qspi.sr.read().busy().bit_is_set() {
+            asm::nop();
+        }
         Ok(())
+    }
+}
+
+fn send_spi_command(command: Command, data: Option<u8>) {
+    let qspi = unsafe { &(*QUADSPI::ptr()) };
+    qspi.dlr.reset();
+
+    if let Some(data) = data {
+        qspi.abr.write(|w| unsafe { w.bits(u32::from(data)) });
+    }
+
+    qspi.ccr.write(|w| unsafe {
+        w.fmode()
+            .bits(QspiMode::IndirectWrite as u8)
+            .imode()
+            .bits(QspiWidth::Single as u8)
+            .instruction()
+            .bits(command as u8);
+
+        if data.is_some() {
+            w.abmode()
+                .bits(QspiWidth::Single as u8)
+                .absize()
+                .bits(QspiSize::OneByte as u8);
+        }
+
+        w
+    });
+
+    while qspi.sr.read().busy().bit_is_set() {
+        asm::nop();
+    }
+}
+
+fn qpi_command(command: Command) {
+    let qspi = unsafe { &(*QUADSPI::ptr()) };
+    qspi.ccr.write(|w| unsafe {
+        w.fmode()
+            .bits(QspiMode::IndirectWrite as u8)
+            .imode()
+            .bits(QspiWidth::Quad as u8)
+            .instruction()
+            .bits(command as u8)
+    });
+
+    while qspi.sr.read().busy().bit_is_set() {
+        asm::nop();
+    }
+}
+
+fn wait_busy() {
+    while {
+        let qspi = unsafe { &(*QUADSPI::ptr()) };
+        qspi.dlr.write(|w| unsafe { w.dl().bits(1 - 1) });
+
+        qspi.ccr.write(|w| unsafe {
+            w.fmode()
+                .bits(QspiMode::IndirectRead as u8)
+                .imode()
+                .bits(QspiWidth::Quad as u8)
+                .dmode()
+                .bits(QspiWidth::Quad as u8)
+                .instruction()
+                .bits(Command::ReadStatusRegister1 as u8)
+        });
+
+        let data = qspi.dr.read().bits();
+
+        while qspi.sr.read().busy().bit_is_set() {
+            asm::nop();
+        }
+
+        data as u8
+    } & 0x01
+        != 0
+    {
+        asm::nop();
     }
 }
